ba4ab55e58a035bdceb861f59597744c4bcaa21b
FabianAN01 <carlosalpna@gmail.com>
2022-08-24 00:36:16 -0500

    Create Operadores.py
    Operaciones Basicas

---
+++ Operadores.py
@@ -0,0 +1,11 @@
+from __future__ import division
+
+
+print("Operaciones Basicas")
+
+suma = 1 + 2
+resta = 55 - 3
+multiplicacion = 2 * 5
+division = 10 / 3
+
+print(suma , resta, multiplicacion, division)
