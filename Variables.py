a0b9febe83954ae97dee7c87a2259e0c712a374d
FabianAN01 <carlosalpna@gmail.com>
2022-08-24 00:20:52 -0500

    Update Variables.py
    métedos upper(), lower() y title()

--- Variables.py
+++ Variables.py
@@ -9,3 +9,15 @@ print(mensaje + 10)
 
 print("Variables String 'Y sus usos'")
 print('Diferentes maneras de usarlo "Uso de las comillas"')
+
+mensaje2 = "modiFICAcion de teXTos"
+
+print (mensaje2)
+
+mensaje2 = mensaje2.title()
+
+print(mensaje2)
+
+print(mensaje2.upper())
+
+print(mensaje2.lower())
