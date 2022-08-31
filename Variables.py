139a49325b4ff777037c49aeabb0df103a6e0918
FabianAN01 <carlosalpna@gmail.com>
2022-08-30 23:35:08 -0500

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -1,15 +1,39 @@
-
+#variables numericas
 mensaje = "Bienvenido al curso de Python"; numeros = 10
 
 print(mensaje)
 
 mensaje = 100
-
+num1 = 101
+num2 = 202
+num3 = num1 + num2 
 print(mensaje + 10)
+print(num3)
+#Variables con textos
+
+print("Probemos" + "Con" + "texto")
+palabra1 = "suma de"
+palabra2 = "cadenas"
+print(palabra1 + palabra2)
+
+palabra3 ="Probemos"
+
+print(palabra3 + "mezclar ")
+
+nombre= "Fabian"
+apellido1 = "Alpizar"
+apellido2 = "Navarro"
+Nomcomp = nombre + apellido1 + apellido2
+print(Nomcomp)
+
+print("04"+"05")
 
 print("Variables String 'Y sus usos'")
 print('Diferentes maneras de usarlo "Uso de las comillas"')
 
+
+
+#Modificacion de texto 
 mensaje2 = "modiFICAcion de teXTos"
 
 print (mensaje2)
