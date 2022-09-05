1e25e0602bd1787553c20fea32ac5c31beaa98a1
FabianAN01 <carlosalpna@gmail.com>
2022-09-05 02:49:42 -0500

    Update Listas_y_usos.py
    Inicio de ejercicios

---
+++ Listas_y_usos.py
@@ -0,0 +1,25 @@
+#Capitulo 10
+smartphones = ['Xiaomi', 'Iphone', 'Huawei', 'Samsung']
+print(smartphones)
+lista1 = ['texto', 10, 55.8, 'texto']
+smartphones = ['Xiaomi', 'Iphone', 'Huawei', 'Samsung']
+print(smartphones[0])
+print(smartphones[1])
+print(smartphones[2])
+print(smartphones[3])
+#Ejercicos cap 10
+colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]
+print(f"En la posicion 3 esta el color: {colores[3]}")
+
+print(f"El color {colores[0]} es la posicion 0 y el {colores[7]} en la posicion 7" )
+
+numlist = ["tres", "dos", "cinco", "cuatro", "uno"]
+print(numlist)
+
+#Capitulo 11
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila','negro', 'rosa','blanco', 'naranja']
+print(f"Vamos a imprimir un color pero ahora desde si valor negatio {colores[-1]}")
+#Ejercicios cap 11
+print(f"Vamos a acceder a los siguientes colores desde su posicion negativa {colores[-1]} el que sigue {colores[-7]} y asi vamos por mas {colores[-5]} otro mas {colores[-7]} y ya el ultimo {colores[-10]} ")
+
+#Capitulo 12
