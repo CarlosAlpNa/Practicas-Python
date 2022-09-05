d4bcf63c0162123ae31576ec5db9a7b615d95d3c
FabianAN01 <carlosalpna@gmail.com>
2022-09-05 02:18:59 -0500

    Update Operadores.py

--- Operadores.py
+++ Operadores.py
@@ -32,3 +32,28 @@ print("La suma de ambos valores es: ")
 print(float1 + float2, )
 print("Muchos decimales no? vamos a redondear")
 print(round(float1 + float2, 2))
+
+#Vamos con los ejercicios 
+#Cap 7
+sumaEjr = 20 + 23 + 44
+print("Veamos si el resultado es el que pide el ejercicio", + sumaEjr  )
+
+restaEjr = 20 - 23 - 84
+print("Veamos si el resultado es el que pide el ejercicio", + restaEjr  )
+
+mulEjr = 10 * 87
+print("Veamos si el resultado es el que pide el ejercicio", + mulEjr  )
+
+
+divEjr = 5000 *  230 / 115000
+print("Veamos si el resultado es el que pide el ejercicio", + divEjr  )
+
+
+opEjer = 10 / 5 + 15 - 17
+print("Veamos si el resultado es el que pide el ejercicio", + opEjer  )
+
+#Cap 8
+op1024 = 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2
+print("Veamos si el resultado es el que pide el ejercicio", + op1024  )
+
+print("Vamos a redondear 17.567383292929200234 para tener solo 5 decimales", + round(17.567383292929200234, 5))
