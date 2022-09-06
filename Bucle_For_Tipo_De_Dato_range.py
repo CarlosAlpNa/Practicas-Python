38d8d5535edb02df39e149e3615a17d85de3d9c8
FabianAN01 <carlosalpna@gmail.com>
2022-09-06 12:49:26 -0500

    Update Bucle_For_Tipo_De_Dato_range.py

--- Bucle_For_Tipo_De_Dato_range.py
+++ Bucle_For_Tipo_De_Dato_range.py
@@ -26,3 +26,18 @@ print("\n")
 print("Capitulo 30")
 for x in range(10):
 	print(x)
+else:
+	print("Se acabo el bucle for...")
+for x in range(5,10):
+	print(x)
+
+numeros = [2,4,8,16,32,64,128]
+for x in range(len(numeros)):
+	print("Número de operacion: ", x," Multiplicacion: ", numeros[x], "Resultados: ", numeros[x] * numeros[x])
+#Ejercicio capitulo 30
+print("Crea un bucle for con un range() que vaya desde el valor 7 hasta el valor 700 en saltos de 100. Basta con que imprimas el valor de cada iteración.")
+for x in range(7, 700, 100):
+	print(x)
+print("\n")
+
+
