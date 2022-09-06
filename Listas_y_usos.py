ab78472e56c787a7f522aa3c2e1905e6e0461c06
FabianAN01 <carlosalpna@gmail.com>
2022-09-05 20:29:26 -0500

    Update Listas_y_usos.py

--- Listas_y_usos.py
+++ Listas_y_usos.py
@@ -1,4 +1,8 @@
 #Capitulo 10
+from asyncio import CancelledError
+from traceback import print_tb
+
+
 smartphones = ['Xiaomi', 'Iphone', 'Huawei', 'Samsung']
 print(smartphones)
 lista1 = ['texto', 10, 55.8, 'texto']
@@ -66,6 +70,58 @@ colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'ros
 print(f"Lista completa:{colores}" )
 colorEl1 = colores.pop(1)
 colorEl2 = colores.pop(7)
+print(f"Lista despues de elimiar algunos elementos:{colores}" )
 colores_eliminados = [colorEl1, colorEl2]
-print(colores_eliminados)
+print(f"Los elementos eliminados son: {colores_eliminados}")
 print("\n")
+
+#Capitulo 15
+colores = ['rojo', 'azul', 'verde', 'amarillo']
+print(f"La lista es:{colores} y le vamos a agregar un color mas en este caso el naranja")
+colores.append('naranja')
+print(f"Despues de agregarlo quedo:{colores}")
+#Ejercicios cap 15
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+print([f"Tenemos la lista{colores} y vamos a agregar nuevos colores a la misma en este caso fuxia y celeste"])
+colores.append('fuxia')
+colores.append('celeste')
+print(f"Despues de agregarle los colores la lisa se ve asi:{colores}")
+print("\n")
+
+#Capitulo 16
+print(f"Ahora podemos espesificar en que lugar podemos agregar los nuevos valores")
+colores = ['rojo', 'azul', 'verde', 'amarillo']
+print(colores)
+print("Ahora agregemos el valor naranja en algun lugar")
+colores.insert(-2,'naranja')
+print(f"Despues de agregarle el color a la lisa se ve asi:{colores}")
+#Ejercicios cap 16
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+print(f"Como ejercicio vamos a agregar  los colores 'magenta' y 'turquesa' utilizando insert(). en la cadena que ya tenemos {colores}")
+colores.insert(-4, 'magenta')
+colores.insert(-1, 'turquesa')
+print(f"Despues de agregarle los colores la lisa se ve asi:{colores}")
+print("\n")
+
+#Capitulo 17
+colores = ['rojo', 'azul', 'verde', 'amarillo']
+print(f"Vamos a acomodar la lista por orden alfabetico:{colores}")
+print(f"Ya ordenada queda asi:")
+print(sorted(colores))
+print("Que tal si ahora la ordenamos alfabeticamente pero al revez")
+print(f"Ya ordenada queda asi:")
+colores.sort(reverse=True)
+print(colores)
+#Ejercicios cap 17 
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón','lila', 'negro', 'rosa', 'blanco', 'naranja']
+print(f"Vamos a ordenar la la lista {colores} \nen orden alfabético descendente (de la letra 'z' a la 'a'). ")
+colores.sort(reverse=True)
+print(f"Vemoas como quedo: {colores}")
+print("\n")
+
+#Capitulo 18
+print(f"Ahora vamos a contar los elementos de la lsitas, tenemos las lista de colores {colores}")
+print("Como se puede observar son muchos elemnetos para contarlos manualmente por que no mejor que lo haga la maquina")
+CanColores = len(colores)
+print (CanColores)
+print("Como podemos observar la maquinas nos ha dicho el numeros de elemntos que estan dentro de la lista y asi podemos guardarlo en una variable para un futuro uso")
