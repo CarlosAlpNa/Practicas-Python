e3182a942c8566146b6d8c67eee1c968f9e38525
FabianAN01 <carlosalpna@gmail.com>
2022-09-06 11:59:48 -0500

    Create Bucle_For_Tipo_De_Dato_range().py

---
+++ Bucle_For_Tipo_De_Dato_range().py
@@ -0,0 +1,13 @@
+#Capitulo 29
+for x in "Python":
+    print(x)
+
+
+cursos = ['Python', 'JavaScript', 'COBOL', 'HTML']
+for x in cursos:
+	print(x)
+
+#Capítulo 30
+
+for x in range(10):
+	print(x)
