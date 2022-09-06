d872dd4c0dacfa57367eb14bbbe2aa5972cfa952
FabianAN01 <carlosalpna@gmail.com>
2022-09-06 00:22:55 -0500

    Update Tips_Para_Condicionales.py

---
+++ Tips_Para_Condicionales.py
@@ -0,0 +1,12 @@
+#Capitulo 26
+x = 100
+y = 200
+z = 300
+if x < y: print("x es menor que y.")
+
+print("Se ejecuta el if.") if x > y  else print("Se ejecuta else") #Otra forma de anotar los ifs y los else
+
+if x < y and z > y: #Usos de los operadores logicos
+    print("Se cumplen las dos condiciones")
+else:
+    print("No se cumplen las dos condiciones")    
