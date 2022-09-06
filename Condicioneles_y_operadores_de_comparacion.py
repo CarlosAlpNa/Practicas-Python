5b4bd53208c09aab59f1c001f135fc8d0f83d64a
FabianAN01 <carlosalpna@gmail.com>
2022-09-05 22:55:13 -0500

    Update Condicioneles_y_operadores_de_comparacion.py

---
+++ Condicioneles_y_operadores_de_comparacion.py
@@ -0,0 +1,38 @@
+
+#Capítulo 21
+print("Operadores de comparación en Python")
+num1 = 10
+num2 = 20 #Aqui debemos de cambiar manualmente los valores de los numeros para ver que condicionales se activan
+if num1 == num2:
+    print("los dos Numeros son iguales")
+
+if  num1  > num2: 
+    print("El numero 1 es Mayor que el numero 2")
+
+if  num1  < num2: 
+    print("El numero 1 es Menor que el numero 2") 
+
+if num1   >= num2:
+    print("El numero 1 Mayor o igual qué 2")
+
+if num1   <= num2:
+    print("El numero 1 Menor o igual qué el numero 2")
+
+if num1   != num2:
+    print("El numero 1 Diferente qué el numero 2 ")  
+
+#Ejercicios cap 21
+print("Vamos a poner en practica algunos condicionales")
+num1 = 15
+num2 = 20
+if num1 != num2:
+	print("Se hace verdad la condicion")
+num1 = 1450
+num2 = 60
+if num1 >= num2:
+	print("Se hace verdad la condicion")
+num1 = 60
+num2 = 60
+if num1 != num2:
+	print("Se hace verdad la condicion")
+print("Con eso vemos algunos usos de los condicionales")
