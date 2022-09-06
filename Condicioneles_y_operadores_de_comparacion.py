39746a47338b26728c4fc21463a3613109c106d9
FabianAN01 <carlosalpna@gmail.com>
2022-09-05 23:27:43 -0500

    Update Condicioneles_y_operadores_de_comparacion.py

--- Condicioneles_y_operadores_de_comparacion.py
+++ Condicioneles_y_operadores_de_comparacion.py
@@ -1,5 +1,8 @@
 
 #Capítulo 21
+
+
+
 print("Operadores de comparación en Python")
 num1 = 10
 num2 = 20 #Aqui debemos de cambiar manualmente los valores de los numeros para ver que condicionales se activan
@@ -36,3 +39,52 @@ num2 = 60
 if num1 != num2:
 	print("Se hace verdad la condicion")
 print("Con eso vemos algunos usos de los condicionales")
+print("\n")
+
+#Capitulo 22
+print("El condicional IF ELSE")
+edad = 20
+if edad >= 18:
+    print("Puedes acceder, eres mayor de edad")
+else:
+    print("No puedes accede, eres menor de edad")    
+#Ejercicios cap 22
+print("Vamos con algun ejercicio") 
+color = "rojo"
+if color != "rojo":
+    print ("El color no es rojo.")
+else:
+    print("El color es rojo.")
+print("Como pudimos ver el codigo corre con normalidad lo cual nos idica que ya no hay ningun arror que arreglar")
+print("\n")
+
+#Capitulo 23
+print("El condicional if elif else e input, entrada de datos")
+edad = int(input("¿Cuál es tu edad?\n"))
+if edad <= 0:
+	print("Nadie puede tener esa edad.")
+elif edad <= 1 and edad < 18:
+	print("Eres menor de edad.")
+elif edad <= 100:
+	print("Eres mayor de edad.")
+else:
+	print("Edad no válida.")
+#Ejercicio capitulo 23
+print("Ahora agregaremos mas rangos de edad") 
+edad = int(input("¿Cuál es tu edad?\n"))
+if edad <= 0:
+	print("Nadie puede tener esa edad.")
+elif edad <= 1 and edad < 18:
+	print("Eres menor de edad.")
+elif edad == 18 and edad <= 45:
+	print("Eres mayor de edad y en buena edad.")
+elif edad <= 100:
+	print("Eres mayor de edad.")
+elif edad > 100 and edad <= 120:
+	print("Eres muy mayor, cuidado al bajar las escaleras")
+else:
+	print("Edad no válida.")
+    
+print("\n")
+
+
